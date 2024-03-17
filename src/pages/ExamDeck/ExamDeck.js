eae24fab042bd2f7a23278c27e7f7857ad6a463c
Swastik Patil <swastik592@gmail.com>
2024-03-17 20:29:39 +0530

    Remove import of UploadPage in ExamDeck.js

--- src/pages/ExamDeck/ExamDeck.js
+++ src/pages/ExamDeck/ExamDeck.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import UploadPage from "./UploadPage";
 import Header from "../../components/Header";
 import styled from "styled-components";
 import { useToast } from "@chakra-ui/react";
